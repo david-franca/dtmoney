ca5682fa8fddfa9b9322bfa7443348f8794821a5
David França <davidemanuel.cf@gmail.com>
2022-05-12 16:20:39 -0300

    Miragejs and ReactModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Modal from "react-modal";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  };
   return (
     <Fragment>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle />
     </Fragment>
   );
